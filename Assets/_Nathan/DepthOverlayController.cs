7d5fd35bd408c667d5dd097db901bd68836dfb2c
unknown <lu.erika58@gmail.com>
2020-10-02 12:48:09 -0700

    le template

--- Assets/_Nathan/DepthOverlayController.cs
+++ Assets/_Nathan/DepthOverlayController.cs
@@ -9,26 +9,23 @@ public class DepthOverlayController : MonoBehaviour
     private LCMListener listener;
 
     public GameObject depthTextGameObject;
-    public GameObject pressureTextGameObject;
-    public GameObject altitudeTextGameObject;
+    public GameObject arrowGameObject;
+
+    public Sprite dotSprite;
 
     private TextMeshProUGUI depth;
-    private TextMeshProUGUI pressure;
-    private TextMeshProUGUI altitude;
+
 
     // Start is called before the first frame update
     void Start()
     {
         listener = listenerGameObject.GetComponent<LCMListener>();
         depth = depthTextGameObject.GetComponent<TextMeshProUGUI>();
-        pressure = pressureTextGameObject.GetComponent<TextMeshProUGUI>();
-        altitude = altitudeTextGameObject.GetComponent<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
     void Update()
     {
         depth.SetText("Depth: " + listener.Depth.ToString());
-        pressure.SetText("Pressure: " + listener.Pressure.ToString());
     }
 }
