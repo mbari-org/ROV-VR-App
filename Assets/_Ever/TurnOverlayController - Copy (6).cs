65e00b14ae49055af8f3cac71c7e1e803c9d3996
EverardoG <everardo.gonzalez@students.olin.edu>
2020-10-22 09:50:42 -0400

    how big is this push going to be

---
+++ Assets/_Ever/TurnOverlayController - Copy (6).cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+// Useful References for Ever:
+// - https://docs.unity3d.com/ScriptReference/Transform.Rotate.html
+// - https://docs.unity3d.com/ScriptReference/GameObject.SetActive.html (for arrows)
+
+public class TurnOverlayController : MonoBehaviour
+{
+    public GameObject listenerGameObject;
+    private LCMListener listener;
+
+    public GameObject compassGameObject;
+    public GameObject turnLabelGameObject;
+
+    private TextMeshProUGUI turnText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        listener = listenerGameObject.GetComponent<LCMListener>();
+        turnText = turnLabelGameObject.GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Debug.Log("Yaw: " + listener.Yaw.ToString());
+        
+        // // turnText.SetText("+" + Math.Round(1.2345678, 1).ToString());
+    }
+}
