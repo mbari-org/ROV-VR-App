f6384a8150cbe5a424f399ec3aa52d418545b2b5
CaliW <cameron.wierzbanowski@students.olin.edu>
2020-12-04 13:56:30 -0500

    add webcam window

---
+++ Assets/Scripts/WebCam.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class WebCam : MonoBehaviour
+{
+    private bool camAvailable;
+    private WebCamTexture cam1;
+    private Texture defaultBackground;
+    public RawImage background;
+    public AspectRatioFitter fit;
+
+    void Start()
+    {
+        defaultBackground = background.texture;
+
+        WebCamDevice[] devices = WebCamTexture.devices;
+        if (devices.Length == 0)
+        {
+            Debug.Log("no camera detected");
+            camAvailable = false;
+            return;
+        }
+
+        for (int i = 0; i < devices.Length; i++)
+        {
+            Debug.Log(devices[i].name);
+            Debug.Log(i);
+        }
+
+        if (cam1 == null)
+        {
+            cam1 = new WebCamTexture(devices[0].name, Screen.width, Screen.height);
+        }
+
+        if (!cam1.isPlaying)
+        {
+            cam1.Play();
+            Debug.Log(cam1.isPlaying);
+        }
+
+        background.texture = cam1;
+
+        camAvailable = true;
+
+    }
+    void Update()
+    {
+        if (!camAvailable)
+            return;
+
+        float ratio = (float)cam1.width / (float)cam1.height;
+        fit.aspectRatio = ratio;
+        float scaleY = cam1.videoVerticallyMirrored ? -1f : 1f;
+        background.rectTransform.localScale = new Vector3(1f, scaleY, 1f);
+
+        int orient = -cam1.videoRotationAngle;
+        background.rectTransform.localEulerAngles = new Vector3(0, 0, orient);
+    }
+}
