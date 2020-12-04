203fca2e145612e1e07801b1a00e37f7bf211918
unknown <lu.erika58@gmail.com>
2020-12-04 13:36:12 -0800

    integrating virtual cam

---
+++ Assets/ProjectVirtualCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectVirtualCamera : MonoBehaviour
+{
+    public Material skyBoxMaterial; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        WebCamTexture webcamTexture = new WebCamTexture();
+        Debug.Log("Looking for camera named: OBS-Camera");
+        webcamTexture.deviceName = "OBS-Camera";
+        skyBoxMaterial.mainTexture = webcamTexture;
+        webcamTexture.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
