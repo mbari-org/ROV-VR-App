9c24aa6d16ea52854bcc05f10f347d98d678b0c3
unknown <lu.erika58@gmail.com>
2020-12-04 11:53:18 -0800

    first

---
+++ Assets/ProjectVirtualCamera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectVirtualCamera : MonoBehaviour
+{
+    public Material skyBoxMaterial; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        WebCamDevice[] devices = WebCamTexture.devices;
+        for (int i = 0; i < devices.Length; i++)
+            Debug.Log(devices[i].name + i);
+        WebCamTexture webcamTexture = new WebCamTexture();
+        Renderer renderer = GetComponent<Renderer>();
+        Debug.Log("Playing Camera: "+ devices[1].name);
+        webcamTexture.deviceName = devices[1].name;
+        skyBoxMaterial.mainTexture = webcamTexture;
+        webcamTexture.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
