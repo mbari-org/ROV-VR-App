a10ca7f1c19b95a07755c9b7aec541f32191dd47
Amy <amyngph@gmail.com>
2020-12-08 16:29:11 -0800

    Refactor webcam again - only use gameobjects

--- Assets/Scripts/WebCam.cs
+++ Assets/Scripts/WebCam.cs
@@ -12,53 +12,30 @@ public class WebCam : MonoBehaviour
 
     private WebCamTexture[] camArray = new WebCamTexture[maxCameras];
 
-    // Will be used to overlay raw image on top of default background
-    private Texture defaultBackground1;
-    private Texture defaultBackground2;
-    private Texture defaultBackground3;
-    private Texture defaultBackground4;
-    private Texture defaultBackground5;
-    private Texture[] defaultBackgroundArray = new Texture[maxCameras];
-
     // Gameobjects that need to be set in inspector menu
-    public RawImage background1;
-    public RawImage background2;
-    public RawImage background3;
-    public RawImage background4;
-    public RawImage background5;
-    private RawImage[] backgroundArray = new RawImage[maxCameras];
+    public GameObject display1;
+    public GameObject display2;
+    public GameObject display3;
+    public GameObject display4;
+    public GameObject display5;
 
-    public AspectRatioFitter fit1;
-    public AspectRatioFitter fit2;
-    public AspectRatioFitter fit3;
-    public AspectRatioFitter fit4;
-    public AspectRatioFitter fit5;
+    private RawImage[] backgroundArray = new RawImage[maxCameras];
     private AspectRatioFitter[] aspectRatioArray = new AspectRatioFitter[maxCameras];
 
     void Start()
     {
         // TODO: Put this in a loop
-        defaultBackgroundArray[0] = defaultBackground1;
-        defaultBackgroundArray[1] = defaultBackground2;
-        defaultBackgroundArray[2] = defaultBackground3;
-        defaultBackgroundArray[3] = defaultBackground4;
-        defaultBackgroundArray[4] = defaultBackground5;
+        backgroundArray[0] = display1.GetComponentInChildren(typeof(RawImage)) as RawImage;
+        backgroundArray[1] = display2.GetComponentInChildren(typeof(RawImage)) as RawImage;
+        backgroundArray[2] = display3.GetComponentInChildren(typeof(RawImage)) as RawImage;
+        backgroundArray[3] = display4.GetComponentInChildren(typeof(RawImage)) as RawImage;
+        backgroundArray[4] = display5.GetComponentInChildren(typeof(RawImage)) as RawImage;
 
-        backgroundArray[0] = background1;
-        backgroundArray[1] = background2;
-        backgroundArray[2] = background3;
-        backgroundArray[3] = background4;
-        backgroundArray[4] = background5;
-
-        aspectRatioArray[0] = fit1;
-        aspectRatioArray[1] = fit2;
-        aspectRatioArray[2] = fit3;
-        aspectRatioArray[3] = fit4;
-        aspectRatioArray[4] = fit5;
-
-        // Set background as our image texture
-        for (int i = 0; i < maxCameras; i++)
-            defaultBackgroundArray[i] = backgroundArray[i].texture;
+        aspectRatioArray[0] = display1.GetComponentInChildren(typeof(AspectRatioFitter)) as AspectRatioFitter;
+        aspectRatioArray[1] = display2.GetComponentInChildren(typeof(AspectRatioFitter)) as AspectRatioFitter;
+        aspectRatioArray[2] = display3.GetComponentInChildren(typeof(AspectRatioFitter)) as AspectRatioFitter;
+        aspectRatioArray[3] = display4.GetComponentInChildren(typeof(AspectRatioFitter)) as AspectRatioFitter;
+        aspectRatioArray[4] = display5.GetComponentInChildren(typeof(AspectRatioFitter)) as AspectRatioFitter;
 
         // Find Webcams
         WebCamDevice[] devices = WebCamTexture.devices;
@@ -74,10 +51,10 @@ public class WebCam : MonoBehaviour
         for (int i = 0; i < devices.Length; i++)
         {
             // Make sure it's not a leapmotion camera
-            //if (devices[i].name != "Leap Dev Kit")
+            if (devices[i].name != "Leap Dev Kit")
 
             // Make sure it's a Blackmagic camera
-            if (devices[i].name.Contains("Black"))
+            //if (devices[i].name.Contains("Black"))
             {
                 Debug.Log(i);
                 Debug.Log(devices[i].name);
@@ -105,8 +82,8 @@ public class WebCam : MonoBehaviour
 
         for (int i = 0; i< numCameras; i++)
         {
-            float ratio = (float)camArray[i].width / (float)camArray[i].height;
-            fit1.aspectRatio = ratio;
+            //float ratio = (float)camArray[i].width / (float)camArray[i].height;
+            //fit1.aspectRatio = ratio;
             float scaleY = camArray[i].videoVerticallyMirrored ? -1f : 1f;
             backgroundArray[i].rectTransform.localScale = new Vector3(1f, scaleY, 1f);
             int orient = -camArray[i].videoRotationAngle;
