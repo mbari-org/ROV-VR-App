0a2abdfb4cee1149012e796370168f71bd0dbe7e
Amy <amyngph@gmail.com>
2021-03-14 16:15:59 -0700

    Implement camera settings menu with joystick

--- Assets/CameraSettingsManager.cs
+++ Assets/CameraSettingsManager.cs
@@ -2,9 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Valve.VR;
 
 public class CameraSettingsManager : MonoBehaviour
 {
+    // Joystick bindings
+    public SteamVR_Action_Vector2 ThumbstickAction;
+    public SteamVR_Input_Sources RHand;
+
     // Skybox we'll be adjusting
     public Material skyboxMaterial;
 
@@ -12,15 +17,56 @@ public class CameraSettingsManager : MonoBehaviour
     public Button InactiveButtonTemplate;
     public Button ActiveButtonTemplate;
 
+    // GUI objects
     public Button LCXButton;
     public Text LCXButtonText;
     public Text LCXValueText;
+
+    public Button LCYButton;
+    public Text LCYButtonText;
+    public Text LCYValueText;
+
+    public Button RCXButton;
+    public Text RCXButtonText;
+    public Text RCXValueText;
+
+    public Button RCYButton;
+    public Text RCYButtonText;
+    public Text RCYValueText;
+
+    public Button LRXButton;
+    public Text LRXButtonText;
+    public Text LRXValueText;
+
+    public Button LRYButton;
+    public Text LRYButtonText;
+    public Text LRYValueText;
+
+    public Button RRXButton;
+    public Text RRXButtonText;
+    public Text RRXValueText;
+
+    public Button RRYButton;
+    public Text RRYButtonText;
+    public Text RRYValueText;
+
+    public Button FisheyeAButton;
+    public Text FisheyeAButtonText;
+    public Text FisheyeAValueText;
+
+    public Button FisheyeBButton;
+    public Text FisheyeBButtonText;
+    public Text FisheyeBValueText;
+
+    public Button FisheyeCButton;
+    public Text FisheyeCButtonText;
+    public Text FisheyeCValueText;
 
     // Keep items in a list to make it easier to use
     private List<Button> ButtonList;
     private List<Text> ButtonTextList;
     private List<Text> ValueTextList;
-    private List<float> CameraSettingsList;
+    private List<string> CameraSettingsList;
 
     int currActive = -1;
 
@@ -29,41 +75,56 @@ public class CameraSettingsManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        ButtonList = new List<Button> { LCXButton };
-        ButtonTextList = new List<Text> { LCXButtonText };
-        ValueTextList = new List<Text> { LCXValueText };
-        CameraSettingsList = new List<float> { 
-            skyboxMaterial.GetFloat("_L_CX"),
-            skyboxMaterial.GetFloat("_L_CY"),
-            skyboxMaterial.GetFloat("_R_CX"),
-            skyboxMaterial.GetFloat("_R_CY"),
-            skyboxMaterial.GetFloat("_L_RX"),
-            skyboxMaterial.GetFloat("_L_RY"),
-            skyboxMaterial.GetFloat("_R_RX"),
-            skyboxMaterial.GetFloat("_R_RY") };
+        ButtonList = new List<Button> { LCXButton, LCYButton, RCXButton,
+            RCYButton, LRXButton, LRYButton, RRXButton, RRYButton, 
+            FisheyeAButton, FisheyeBButton, FisheyeCButton };
 
-        LCXButton.onClick.AddListener(delegate
-            { SettingsButtonCallback(0); });
+        ButtonTextList = new List<Text> { LCXButtonText, LCYButtonText, RCXButtonText, 
+            RCYButtonText, LRXButtonText, LRYButtonText, RRXButtonText, RRYButtonText, 
+            FisheyeAButtonText, FisheyeBButtonText, FisheyeCButtonText };
 
+        ValueTextList = new List<Text> { LCXValueText, LCYValueText, RCXValueText, 
+            RCYValueText, LRXValueText, LRYValueText, RRXValueText, RRYValueText, 
+            FisheyeAValueText, FisheyeBValueText, FisheyeCValueText };
 
-        //L_CX_slider.value = skyboxMaterial.GetFloat("_L_CX");
-        //L_CY_slider.value = skyboxMaterial.GetFloat("_L_CY");
-        //R_CX_slider.value = skyboxMaterial.GetFloat("_R_CX");
-        //R_CY_slider.value = skyboxMaterial.GetFloat("_R_CY");
-        //L_RX_slider.value = skyboxMaterial.GetFloat("_L_RX");
-        //L_RY_slider.value = skyboxMaterial.GetFloat("_L_RY");
-        //R_RX_slider.value = skyboxMaterial.GetFloat("_R_RX");
-        //R_RY_slider.value = skyboxMaterial.GetFloat("_R_RY");
+        CameraSettingsList = new List<string> { "_L_CX", "_L_CY", "_R_CX",
+            "_R_CY", "_L_RX", "_L_RY", "_R_RX", "_R_RY", 
+            "_a", "_b", "_c" };
 
+        LCXButton.onClick.AddListener(delegate { SettingsButtonCallback(0); });
+        LCYButton.onClick.AddListener(delegate { SettingsButtonCallback(1); });
+        RCXButton.onClick.AddListener(delegate { SettingsButtonCallback(2); });
+        RCYButton.onClick.AddListener(delegate { SettingsButtonCallback(3); });
+        LRXButton.onClick.AddListener(delegate { SettingsButtonCallback(4); });
+        LRYButton.onClick.AddListener(delegate { SettingsButtonCallback(5); });
+        RRXButton.onClick.AddListener(delegate { SettingsButtonCallback(6); });
+        RRYButton.onClick.AddListener(delegate { SettingsButtonCallback(7); });
+        FisheyeAButton.onClick.AddListener(delegate { SettingsButtonCallback(8); });
+        FisheyeBButton.onClick.AddListener(delegate { SettingsButtonCallback(9); });
+        FisheyeCButton.onClick.AddListener(delegate { SettingsButtonCallback(10); });
+
+        // Populate GUI with initial values
+        // TODO: Read these from a save file
+        int i = 0;
+        foreach (string settingName in CameraSettingsList)
+        {
+            float value = skyboxMaterial.GetFloat(settingName);
+            skyboxMaterial.SetFloat(settingName, value);
+            ValueTextList[i].text = settingName + ": " + value.ToString();
+            i += 1;
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (currActive != -1)
         {
             // Use right joystick to update this value
-            //print(CameraSettingsList[currActive]);
+            Vector2 joystickInput = ThumbstickAction.GetAxis(RHand);
+            string settingName = CameraSettingsList[currActive];
+            float newValue = skyboxMaterial.GetFloat(settingName) + joystickInput.y * 0.0003f; // 0.1 to adjust sensitivity
+            skyboxMaterial.SetFloat(settingName, newValue);
+            ValueTextList[currActive].text = settingName + ": " + newValue.ToString();
         }
     }
 
--- Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -105,6 +105,14 @@ namespace Valve.VR
             }
         }
         
+        public virtual SteamVR_Action_Vector2 JoystickMove
+        {
+            get
+            {
+                return SteamVR_Actions.default_JoystickMove;
+            }
+        }
+        
         public virtual SteamVR_Action_Vibration Haptic
         {
             get
