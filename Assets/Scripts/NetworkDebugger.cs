5006a4b0ae0454c3aee27239588600e6184789a9
Amy <amyngph@gmail.com>
2021-03-26 10:47:51 -0700

    Add time and received messages display to LCM debugger

--- Assets/Scripts/NetworkDebugger.cs
+++ Assets/Scripts/NetworkDebugger.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 public class NetworkDebugger : MonoBehaviour
 {
-    public DataCanvas outputCanvas;
+    public LCMDebugger outputCanvas;
 
     // Start is called before the first frame update
     void Start()
