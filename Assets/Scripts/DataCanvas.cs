afc00daad3db91dd112553703e02440222199128
Amy <amyngph@gmail.com>
2021-03-26 09:44:23 -0700

    Add LCM debugger to popup window

---
+++ Assets/Scripts/DataCanvas.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+public class DataCanvas : MonoBehaviour
+{
+    public LCMListener listener;
+    public Text deltaText;
+    public Text depthText;
+    public Text rollText;
+    public Text pitchText;
+    public Text yawText;
+    public Text rovLatText;
+    public Text rovLonText;
+    public Text shipLatText;
+    public Text shipLonText;
+    public Text clumpLatText;
+    public Text clumpLonText;
+
+    public Text debugText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        double deltaValue = Math.Round(listener.ClumpDelta, 3);
+        deltaText.text = "Clump Delta: " + deltaValue.ToString();
+
+        double depthValue = Math.Round(listener.Depth, 3);
+        depthText.text = "Depth: " + depthValue.ToString();
+
+        double rollValue = Math.Round(listener.Roll, 3);
+        rollText.text = "Roll: " + rollValue.ToString();
+
+        double pitchValue = Math.Round(listener.Pitch, 3);
+        pitchText.text = "Pitch: " + pitchValue.ToString();
+
+        double yawValue = Math.Round(listener.Yaw, 3);
+        yawText.text = "Yaw: " + yawValue.ToString();
+
+        double rovLatValue = Math.Round(listener.ROVLat, 3);
+        rovLatText.text = "ROV Lat: " + rovLatValue.ToString();
+
+        double rovLonValue = Math.Round(listener.ROVLon, 3);
+        rovLonText.text = "ROV Lon: " + rovLonValue.ToString();
+
+        double shipLatValue = Math.Round(listener.ShipLat, 3);
+        shipLatText.text = "Ship Lat: " + shipLatValue.ToString();
+
+        double shipLonValue = Math.Round(listener.ShipLon, 3);
+        shipLonText.text = "Ship Lon: " + shipLonValue.ToString();
+
+        double clumpLatValue = Math.Round(listener.ClumpLat, 3);
+        clumpLatText.text = "Clump Lat: " + clumpLatValue.ToString();
+
+        double clumpLonValue = Math.Round(listener.ClumpLon, 3);
+        clumpLonText.text = "Clump Lon: " + clumpLonValue.ToString();
+    }
+
+    public void printDebug(String msg)
+    {
+        debugText.text = debugText.text + "\n" + msg;
+    }
+}
---
+++ Assets/Scripts/NetworkDebugger.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.NetworkInformation;
+using UnityEngine;
+
+public class NetworkDebugger : MonoBehaviour
+{
+    public DataCanvas outputCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DisplayDnsConfiguration();
+        IPGlobalProperties properties = IPGlobalProperties.GetIPGlobalProperties();
+        IPEndPoint[] tcpEndPoints = properties.GetActiveTcpListeners();
+        IPEndPoint[] udpEndPoints = properties.GetActiveUdpListeners();
+        TcpConnectionInformation[] activeEndPoints = properties.GetActiveTcpConnections();
+
+
+        //Check port for each etc
+        //print(tcpEndPoints.Port);
+        //print(udpEndPoints.Port);
+        //print(tcpEndPoints.Port);
+
+        foreach (IPEndPoint endpoint in udpEndPoints)
+        {
+            if (endpoint.Port == 7667)
+            {
+                outputCanvas.printDebug("Address");
+                outputCanvas.printDebug(endpoint.Address.ToString());
+                outputCanvas.printDebug("Port");
+                outputCanvas.printDebug(endpoint.Port.ToString());
+            }
+
+        }
+
+        //foreach (IPEndPoint endpoint in tcpEndPoints)
+        //{
+        //    print("Address");
+        //    print(endpoint.Address);
+        //    print("Port");
+        //    print(endpoint.Port);
+        //}
+        ////activeEndPoints.LocalEndPoint.Port
+        //print("UDP!!");
+        //foreach (IPEndPoint endpoint in udpEndPoints)
+        //{
+        //    print("Address");
+        //    print(endpoint.Address);
+        //    print("Port");
+        //    print(endpoint.Port);
+        //}
+
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void DisplayDnsConfiguration()
+    {
+        NetworkInterface[] adapters = NetworkInterface.GetAllNetworkInterfaces();
+        foreach (NetworkInterface adapter in adapters)
+        {
+            IPInterfaceProperties properties = adapter.GetIPProperties();
+            outputCanvas.printDebug(adapter.Description.ToString());
+            //print(properties.DnsSuffix);
+            //print(properties.IsDnsEnabled);
+            //print(properties.IsDynamicDnsEnabled);
+            MulticastIPAddressInformationCollection allinfo = properties.MulticastAddresses;
+
+            //print("HERE!!!");
+            //print(allinfo.Count);
+            //foreach (IPAddressInformation info in allinfo)
+            //{
+
+            //    print(info.Address);
+
+            //}
+            //adapter.
+            //print(DNS suffix"
+            //    properties.DnsSuffix);
+            //print("  DNS enabled ............................. : {0}",
+            //    properties.IsDnsEnabled);
+            //print("  Dynamically configured DNS .............. : {0}",
+            //    properties.IsDynamicDnsEnabled);
+        }
+    }
+}
