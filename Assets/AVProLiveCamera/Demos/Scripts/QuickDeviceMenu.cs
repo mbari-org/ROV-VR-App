56bf0b31535f8b90807335583d894de25dbf0695
Amy <amyngph@gmail.com>
2021-04-22 17:53:10 -0700

    Implement left/right camera settings

--- Assets/AVProLiveCamera/Demos/Scripts/QuickDeviceMenu.cs
+++ Assets/AVProLiveCamera/Demos/Scripts/QuickDeviceMenu.cs
@@ -15,7 +15,7 @@ public class QuickDeviceMenu : MonoBehaviour
 
     private void Update()
 	{
-		if (Input.GetKeyDown(KeyCode.Space))
+		if (Input.GetKeyDown(KeyCode.Tab))
 		{
 			ToggleVisible();
 		}
@@ -39,13 +39,16 @@ public class QuickDeviceMenu : MonoBehaviour
 		if (_liveCameraManager.NumDevices > 0)
 		{
 			GUILayout.BeginArea(new Rect(0f, 0f, Screen.width, Screen.height));
-			if (GUILayout.Button("Press SPACE to hide/show QuickDeviceMenu (improves performance)", GUILayout.ExpandWidth(false)))
+            if (GUILayout.Button("Press TAB to hide/show QuickDeviceMenu (improves performance)", GUILayout.ExpandWidth(false)))
 			{
 				ToggleVisible();
 			}
-			GUILayout.EndArea();
+            GUILayout.Button("SHIFT-click to make right camera selection", GUILayout.ExpandWidth(false));
+
+            GUILayout.EndArea();
 
             // NOTE: This is just a spacing element to leave space for the above message
+            GUILayout.Label(" ");
             GUILayout.Label(" ");
 
             GUILayout.BeginHorizontal();
@@ -56,15 +59,18 @@ public class QuickDeviceMenu : MonoBehaviour
             for (int i = 0; i < _liveCameraManager.NumDevices; i++)
             {
                 string name = _liveCameraManager.GetDevice(i).Name;
-                // TODO: Fix bug where right camera index starts at 0
+
                 GUI.color = Color.white;
                 if (_leftLiveCamera.Device != null && _leftLiveCamera.Device.IsRunning)
                 {
                     if (_leftLiveCamera.Device.DeviceIndex == i)
                     {
-                        GUI.color = Color.red;
+                        GUI.color = Color.cyan;
                     } 
-                    else if (_rightLiveCamera.Device.DeviceIndex == i)
+                }
+                if (_rightLiveCamera.Device != null && _rightLiveCamera.Device.IsRunning)
+                {
+                    if (_rightLiveCamera.Device.DeviceIndex == i)
                     {
                         GUI.color = Color.green;
                     }
@@ -88,112 +94,10 @@ public class QuickDeviceMenu : MonoBehaviour
             }
             GUI.color = Color.white;
             GUILayout.EndVertical();
-
-            //if (_leftLiveCamera.Device != null && _leftLiveCamera.Device.IsRunning)
-            //{
-            //    GUILayout.BeginVertical();
-            //    GUILayout.Button("RESOLUTION");
-            //    _scrollResolutions = GUILayout.BeginScrollView(_scrollResolutions, false, false, GUIStyle.none, GUI.skin.verticalScrollbar);
-            //    List<string> usedNames = new List<string>(32);
-            //    for (int i = 0; i < _leftLiveCamera.Device.NumModes; i++)
-            //    {
-            //        AVProLiveCameraDeviceMode mode = _leftLiveCamera.Device.GetMode(i);
-            //        string name = string.Format("{0}x{1}", mode.Width, mode.Height);
-            //        if (!usedNames.Contains(name))
-            //        {
-            //            GUI.color = Color.white;
-            //            if (_leftLiveCamera.Device.CurrentWidth == mode.Width && _leftLiveCamera.Device.CurrentHeight == mode.Height)
-            //            {
-            //                GUI.color = Color.green;
-            //            }
-
-            //            usedNames.Add(name);
-            //            if (GUILayout.Button(name))
-            //            {
-            //                _leftLiveCamera._modeSelection = AVProLiveCamera.SelectModeBy.Index;
-            //                _leftLiveCamera._desiredModeIndex = i;
-            //                _leftLiveCamera.Begin();
-            //            }
-            //        }
-            //    }
-            //    GUI.color = Color.white;
-            //    GUILayout.EndScrollView();
-            //    GUILayout.EndVertical();
-
-            //    // Select frame rate
-            //    usedNames.Clear();
-            //    GUILayout.BeginVertical();
-            //    GUILayout.Button("FPS");
-            //    for (int i = 0; i < _leftLiveCamera.Device.NumModes; i++)
-            //    {
-            //        string matchName = string.Format("{0}x{1}", _leftLiveCamera.Device.CurrentWidth, _leftLiveCamera.Device.CurrentHeight);
-
-            //        AVProLiveCameraDeviceMode mode = _leftLiveCamera.Device.GetMode(i);
-
-            //        string resName = string.Format("{0}x{1}", mode.Width, mode.Height);
-            //        if (resName == matchName)
-            //        {
-            //            foreach (float frameRate in mode.FrameRates)
-            //            {
-            //                string name = string.Format("{0}", frameRate.ToString("F2"));
-            //                if (!usedNames.Contains(name))
-            //                {
-            //                    GUI.color = Color.white;
-            //                    if (_leftLiveCamera.Device.CurrentFrameRate.ToString("F2") == frameRate.ToString("F2"))
-            //                    {
-            //                        GUI.color = Color.green;
-            //                    }
-
-            //                    usedNames.Add(name);
-            //                    if (GUILayout.Button(name))
-            //                    {
-            //                        _leftLiveCamera._modeSelection = AVProLiveCamera.SelectModeBy.Index;
-            //                        _leftLiveCamera._desiredModeIndex = i;
-            //                        _leftLiveCamera._desiredFrameRate = frameRate;
-            //                        _leftLiveCamera.Begin();
-            //                    }
-            //                }
-            //            }
-            //        }
-            //    }
-            //    GUI.color = Color.white;
-            //    GUILayout.EndVertical();
-
-            //    // Select format
-            //    usedNames.Clear();
-            //    GUILayout.BeginVertical();
-            //    GUILayout.Button("FORMAT");
-            //    for (int i = 0; i < _leftLiveCamera.Device.NumModes; i++)
-            //    {
-            //        string matchName = string.Format("{0}x{1}", _leftLiveCamera.Device.CurrentWidth, _leftLiveCamera.Device.CurrentHeight);//, _leftLiveCamera.Device.CurrentFrameRate.ToString("F2"));
-
-            //        AVProLiveCameraDeviceMode mode = _leftLiveCamera.Device.GetMode(i);
-
-            //        string resName = string.Format("{0}x{1}", mode.Width, mode.Height);//, mode.FPS.ToString("F2"));
-            //        if (resName == matchName)
-            //        {
-            //            string name = string.Format("{0}", mode.Format);
-            //            if (!usedNames.Contains(name))
-            //            {
-            //                GUI.color = Color.white;
-            //                if (_leftLiveCamera.Device.CurrentDeviceFormat == mode.Format)
-            //                {
-            //                    GUI.color = Color.green;
-            //                }
-
-            //                usedNames.Add(name);
-            //                if (GUILayout.Button(name))
-            //                {
-            //                    _leftLiveCamera._modeSelection = AVProLiveCamera.SelectModeBy.Index;
-            //                    _leftLiveCamera._desiredModeIndex = i;
-            //                    _leftLiveCamera.Begin();
-            //                }
-            //            }
-            //        }
-            //    }
-            //    GUI.color = Color.white;
-            //    GUILayout.EndVertical();
-            //}
+            
+            DisplaySettings(_rightLiveCamera, "RIGHT");
+            DisplaySettings(_leftLiveCamera, "LEFT");
+            
             GUILayout.EndHorizontal();
         }
 		else
@@ -201,4 +105,123 @@ public class QuickDeviceMenu : MonoBehaviour
 			GUILayout.Label("No webcam / capture devices found");
 		}
 	}
+
+    private void DisplaySettings(AVProLiveCamera liveCamera, string side)
+    {
+        Color activeButtonColor;
+        if (side == "RIGHT")
+        {
+            activeButtonColor = Color.green;
+        }
+        else // side == "LEFT"
+        {
+            activeButtonColor = Color.cyan;
+        }
+
+        if (liveCamera.Device != null && liveCamera.Device.IsRunning)
+        {
+            GUILayout.BeginVertical();
+            GUILayout.Button(side + " RESOLUTION");
+            _scrollResolutions = GUILayout.BeginScrollView(_scrollResolutions, false, false, GUIStyle.none, GUI.skin.verticalScrollbar);
+            List<string> usedNames = new List<string>(32);
+            for (int i = 0; i < liveCamera.Device.NumModes; i++)
+            {
+                AVProLiveCameraDeviceMode mode = liveCamera.Device.GetMode(i);
+                string name = string.Format("{0}x{1}", mode.Width, mode.Height);
+                if (!usedNames.Contains(name))
+                {
+                    GUI.color = Color.white;
+                    if (liveCamera.Device.CurrentWidth == mode.Width && liveCamera.Device.CurrentHeight == mode.Height)
+                    {
+                        GUI.color = activeButtonColor;
+                    }
+
+                    usedNames.Add(name);
+                    if (GUILayout.Button(name))
+                    {
+                        liveCamera._modeSelection = AVProLiveCamera.SelectModeBy.Index;
+                        liveCamera._desiredModeIndex = i;
+                        liveCamera.Begin();
+                    }
+                }
+            }
+            GUI.color = Color.white;
+            GUILayout.EndScrollView();
+            GUILayout.EndVertical();
+
+            // Select frame rate
+            usedNames.Clear();
+            GUILayout.BeginVertical();
+            GUILayout.Button(side + " FPS");
+            for (int i = 0; i < liveCamera.Device.NumModes; i++)
+            {
+                string matchName = string.Format("{0}x{1}", liveCamera.Device.CurrentWidth, liveCamera.Device.CurrentHeight);
+
+                AVProLiveCameraDeviceMode mode = liveCamera.Device.GetMode(i);
+
+                string resName = string.Format("{0}x{1}", mode.Width, mode.Height);
+                if (resName == matchName)
+                {
+                    foreach (float frameRate in mode.FrameRates)
+                    {
+                        string name = string.Format("{0}", frameRate.ToString("F2"));
+                        if (!usedNames.Contains(name))
+                        {
+                            GUI.color = Color.white;
+                            if (liveCamera.Device.CurrentFrameRate.ToString("F2") == frameRate.ToString("F2"))
+                            {
+                                GUI.color = activeButtonColor;
+                            }
+
+                            usedNames.Add(name);
+                            if (GUILayout.Button(name))
+                            {
+                                liveCamera._modeSelection = AVProLiveCamera.SelectModeBy.Index;
+                                liveCamera._desiredModeIndex = i;
+                                liveCamera._desiredFrameRate = frameRate;
+                                liveCamera.Begin();
+                            }
+                        }
+                    }
+                }
+            }
+            GUI.color = Color.white;
+            GUILayout.EndVertical();
+
+            // Select format
+            usedNames.Clear();
+            GUILayout.BeginVertical();
+            GUILayout.Button(side + " FORMAT");
+            for (int i = 0; i < liveCamera.Device.NumModes; i++)
+            {
+                string matchName = string.Format("{0}x{1}", liveCamera.Device.CurrentWidth, liveCamera.Device.CurrentHeight);//, liveCamera.Device.CurrentFrameRate.ToString("F2"));
+
+                AVProLiveCameraDeviceMode mode = liveCamera.Device.GetMode(i);
+
+                string resName = string.Format("{0}x{1}", mode.Width, mode.Height);//, mode.FPS.ToString("F2"));
+                if (resName == matchName)
+                {
+                    string name = string.Format("{0}", mode.Format);
+                    if (!usedNames.Contains(name))
+                    {
+                        GUI.color = Color.white;
+                        if (liveCamera.Device.CurrentDeviceFormat == mode.Format)
+                        {
+                            GUI.color = activeButtonColor;
+                        }
+
+                        usedNames.Add(name);
+                        if (GUILayout.Button(name))
+                        {
+                            liveCamera._modeSelection = AVProLiveCamera.SelectModeBy.Index;
+                            liveCamera._desiredModeIndex = i;
+                            liveCamera.Begin();
+                        }
+                    }
+                }
+            }
+            GUI.color = Color.white;
+            GUILayout.EndVertical();
+        }
+    }
 }
