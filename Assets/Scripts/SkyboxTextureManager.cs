079889a5c30e32021840dacabf48caca09e2515e
Amy <amyngph@gmail.com>
2021-04-22 18:02:12 -0700

    Update skybox with new settings

---
+++ Assets/Scripts/SkyboxTextureManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RenderHeads.Media.AVProLiveCamera;
+
+public class SkyboxTextureManager : MonoBehaviour
+{
+    public AVProLiveCamera leftAVProCamera;
+    public AVProLiveCamera rightAVProCamera;
+    public Material skyboxMaterial;
+
+    void Update()
+    {
+        skyboxMaterial.SetTexture(Shader.PropertyToID("_LTex"), leftAVProCamera.OutputTexture);
+        skyboxMaterial.SetTexture(Shader.PropertyToID("_RTex"), rightAVProCamera.OutputTexture);
+    }
+}
